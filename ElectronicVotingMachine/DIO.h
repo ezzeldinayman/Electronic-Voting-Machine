4d3eaafbfd6b46a16a06867a474dcdf08d425a93
Ezz Eldin Ayman <ezz_ayman@hotmail.com>
2022-03-04 19:51:40 +0200

    Update DIO.h

--- ElectronicVotingMachine/DIO.h
+++ ElectronicVotingMachine/DIO.h
@@ -10,22 +10,30 @@
 #define DIO_H_
 #include "std_macros.h"
 
-void DIO_vSetPinDir(unsigned char portname,unsigned char pinnumber,unsigned char direction);
 //Sets pin data direction (direction 1 = output pin / direction 0 = input pin) (DDRx)
-void DIO_vWrite(unsigned char portname, unsigned char pinnumber, unsigned char val);
+void DIO_vSetPinDir(unsigned char portname,unsigned char pinnumber,unsigned char direction);
+
 //Write an output on a pin (val=1 LOGIC HIGH, val=0 LOGIC LOW) (PORTx)
-void DIO_vToggle(unsigned char portname, unsigned char pinnumber);
+void DIO_vWrite(unsigned char portname, unsigned char pinnumber, unsigned char val);
+
 //Toggles Digital output on an output pin.
-unsigned char DIO_u8read(unsigned char portname, unsigned char pinnumber);
+void DIO_vToggle(unsigned char portname, unsigned char pinnumber);
+
 //reads input on a pin (PINx)
+unsigned char DIO_u8read(unsigned char portname, unsigned char pinnumber);
+
+//sets a whole port as a an output port (direction 1) or input port (direction 0)
 void DIO_vSETPORTDir(unsigned char portname, unsigned char direction);
-//sets a whole port as a an output port (direction 1) or input port (direction 0) 
+
+//writes Digital value on an output WHOLE Port (LOGIC HIGH OR LOW) 
 void DIO_vWritePORT(unsigned char portname, unsigned char val);
-//writes Digital value on an output WHOLE Port (LOGIC HIGH OR LOW)
-void DIO_vTOGPORT(unsigned char portname);
+
 //Toggles value on a whole output port
-unsigned char DIO_u8ReadPort(unsigned char portname);
+void DIO_vTOGPORT(unsigned char portname);
+
 //reads value written on whole input port (PINx)
+unsigned char DIO_u8ReadPort(unsigned char portname);
+
 
 void DIO_vWriteHighNibble(unsigned char portname, unsigned char val);
 void DIO_vWriteLowNibble(unsigned char portname, unsigned char val);
